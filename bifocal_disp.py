3938b1178624279857d875b989f1c1074f20a760
bobtwinkles <srkoser+GitHub@gmail.com>
2014-05-16 16:55:41 -0400

    initial commit

---
+++ bifocal_disp.py
@@ -0,0 +1,10 @@
+from util.windowmanagement import create_window, main_loop
+from opengl import *
+
+def disp():
+    glClear(GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT)
+
+def init():
+    pass
+
+create_window(

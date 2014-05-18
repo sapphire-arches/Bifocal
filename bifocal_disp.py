20eeae3d9b89caf6468f5dfc507451b8334856ab
bobtwinkles <srkoser+GitHub@gmail.com>
2014-05-17 22:55:59 -0400

    add time tracking/metrics

--- bifocal_disp.py
+++ bifocal_disp.py
@@ -2,16 +2,18 @@ from pyoogl.windowmanagement import Window
 from OpenGL.GL import *
 from sys import argv
 from PIL import Image
+from time import time
 
 def disp():
     glClear(GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT)
 
 MAX_DIFF = 5
+WINDOW_RADIUS = 1
 
 def compute_region_diff(imgr, imgl, x, y, d):
     diff = 0
-    for xx in range(-2, 2):
-        for yy in range(-2, 2):
+    for xx in range(-WINDOW_RADIUS, WINDOW_RADIUS):
+        for yy in range(-WINDOW_RADIUS, WINDOW_RADIUS):
             p1 = (x + xx, y + yy)
             p2 = (x + xx + d, y + yy)
             dd = imgr.getpixel(p1) - imgl.getpixel(p2)
@@ -28,14 +30,20 @@ def compute_dif(imgr, imgl):
 
     print(imgr.size, imgl.size)
 
-    for x in range(2 * MAX_DIFF, size[0] - MAX_DIFF):
-        for y in range(2 * MAX_DIFF, size[1] - MAX_DIFF):
+    pixel_count = 0
+    skiped_count = 0
+
+    tick = time()
+    last_print = tick
+
+    for x in range(MAX_DIFF + WINDOW_RADIUS, size[0] - WINDOW_RADIUS):
+        for y in range(MAX_DIFF + WINDOW_RADIUS, size[1] - WINDOW_RADIUS):
             p = (x, y)
             img_diff = (imgr.getpixel(p) - imgl.getpixel(p) + 255) // 2
             diff.putpixel(p, img_diff)
             min_diff = 1 << 31 # really friggin big number
             min_diff_pos = 0
-            if abs(img_diff) > 20:
+            if abs(img_diff) > 100:
                 for d in range(-MAX_DIFF, 1):
                     region_diff = compute_region_diff(imgr, imgl, x, y, d)
                     if region_diff < min_diff:
@@ -43,14 +51,22 @@ def compute_dif(imgr, imgl):
                         min_diff = region_diff
             else:
                 #assume noe difference
+                skiped_count += 1
                 min_diff_pos = 0
             min_diff_pos = -min_diff_pos
             depth.putpixel(p, min_diff_pos * 20)
-        print (x)
+            pixel_count += 1
+        if time() - last_print > 1:
+            print(x)
+            last_print = time()
 
+    tock = time()
 
     diff.save("testdiff.png", "png")
     depth.save("testdepth.png", "png")
+
+    print("Skipped %%%1.2d of pixels. Throughput was %3.2d pixels per second" % (100 * skiped_count / pixel_count, pixel_count / (tock - tick)))
+    return depth
 
 
 def init():
@@ -58,7 +74,7 @@ def init():
     imgl = Image.open(argv[2]).convert('L')
     compute_dif(imgr, imgl)
 
-win = Window("bifocal_display", display_func=disp)
-win.initialize(argv)
+# win = Window("bifocal_display", display_func=disp)
+# win.initialize(argv)
 init()
-win.main_loop()
+# win.main_loop()
